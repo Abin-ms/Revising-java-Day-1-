bf695a4599025519ad7b0b0428c5d1774b0e303c
Abin-ms <abinn.mss@gmail.com>
2026-05-18 22:50:45 +0530

    Add files via upload

---
+++ jaavaa/paraconst.java
@@ -0,0 +1,27 @@
+public class paraconst {
+    public static void main (String []args){
+        aaa a1 = new aaa();
+        // bbb b1 = new bbb();
+        System.out.println(aaa.rr);
+        System.out.println(aaa.hh);
+        // System.out.println(bbb.rr);
+        // System.out.println(bbb.hh);
+    }
+}
+class aaa{
+    int rr;
+    String hh;
+void aaa(int r,String h){
+
+System.out.println(+rr +hh);
+}
+}
+// class bbb{
+//     int hh;
+//     String rr;
+//     void bbb(int h,String r){
+//         hh=h;
+//         rr=r;
+//     }
+
+// }
