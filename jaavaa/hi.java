bbe64743019bd5c1354613a8a4a657ab26a46697
Abin-ms <abinn.mss@gmail.com>
2026-06-10 12:40:41 +0530

    Refactor output message in hi.java

--- jaavaa/hi.java
+++ jaavaa/hi.java
@@ -1,6 +1,6 @@
 // import java.util.*;
 public class hi {
     public static void main(String[] args){
-    System.out.println("helllo bitchass nibhaass enter");
+    System.out.println("helllo  nibhaass enter");
     }
 }
